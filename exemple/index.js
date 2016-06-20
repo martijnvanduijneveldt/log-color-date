85d6ac979d44b66007a763420f8ec643f148a0ee
martijnvanduijnveldt <martijnvanduijneveldt@gmail.com>
2016-06-20 17:48:07 +0200

    1.0.1 - Fixed when log used %, updated colors to light versions

--- exemple/index.js
+++ exemple/index.js
@@ -5,3 +5,8 @@ console.log("console.log")
 console.warn("console.warn")
 console.error("console.error")
 console.info("console.info")
+
+console.log("%s %d","test",1)
+console.warn("%s %d","test",1)
+console.error("%s %d","test",1)
+console.info("%s %d","test",1)
